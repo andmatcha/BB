fe6cbdbf0f1b939496b0ecfe50347e4123cf08ea
andmatcha <jj.mcr185@gmail.com>
2022-08-21 01:13:04 +0900

    ボタンを作成

--- src/html/index.php
+++ src/html/index.php
@@ -1,9 +1,11 @@
 <?php
 
 require('../app/pages/Layouts.php');
+require('../app/pages/Components.php');
 
 layoutStart('トップ | ひとこと掲示板', 'ひとこと掲示板');
 ?>
 <p>メッセージをどうぞ</p>
 <?php
+cMainButton('投稿');
 layoutEnd();
