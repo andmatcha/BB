63b39dc84d2a667f3c6e8d94cb35336edbb17941
andmatcha <jj.mcr185@gmail.com>
2022-08-21 01:29:38 +0900

    フォームのパーツを作りました

--- src/app/pages/Components.php
+++ src/app/pages/Components.php
@@ -1,13 +1,19 @@
 <?php
 
-function cMainButton($text, $attributes = [])
+function convertAttributesArrayToString($attributes)
 {
     $attributesString = '';
-    foreach($attributes as $key => $value) {
+    foreach ($attributes as $key => $value) {
         $attributesString .= ' ' . $key . '="' . $value . '"';
     }
+    return $attributesString;
+}
+
+function cMainButton($text, $attributes = [])
+{
+    $attributesString = convertAttributesArrayToString($attributes);
 ?>
-    <button class="c_main_button"<?= $attributesString ?>><?= $text ?></button>
+    <button class="c_main_button" <?= $attributesString ?>><?= $text ?></button>
     <style>
         .c_main_button {
             background-color: #6EAACC;
@@ -19,6 +25,35 @@ function cMainButton($text, $attributes = [])
             padding: 0 48px;
             height: 64px;
             font-size: 20px;
+        }
+    </style>
+<?php
+}
+
+function cInputSection($label, $name, $type,  $attributes = [])
+{
+    $attributesString = convertAttributesArrayToString($attributes);
+?>
+    <div class="c_input_section">
+        <label for="<?= $name ?>" class="c_input_label"><?= $label ?></label>
+        <input name="<?= $name ?>" type="<?= $type ?>" class="c_input">
+    </div>
+    <style>
+        .c_input_section {
+            display: flex;
+            flex-direction: column;
+            gap: 8px;
+        }
+        .c_input_label {
+            font-size: 16px;
+            line-height: 24px;
+        }
+
+        .c_input {
+            background-color: #FFF;
+            border-radius: 8px;
+            width: 100%;
+            height: 36px;
         }
     </style>
 <?php
